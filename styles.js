beb72088f9586d4c7c8171c34f9a2fd218819f8b
Sam Inyang <saminyang09@gmail.com>
2020-04-21 12:24:42 -0400

    adding colors to styles export

--- styles.js
+++ styles.js
@@ -6,6 +6,7 @@ import { light } from "styles/theme/light"
 export const THEMES = { light, dark }
 export * from "styles/theme/Theme.type"
 
+export * from "styles/colors"
 export * from "styles/spacings"
 export * from "styles/typography"
 export * from "styles/types"
