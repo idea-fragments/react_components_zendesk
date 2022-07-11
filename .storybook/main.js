f55cb1284db6cf006256c6430958b06acb6490d0
Sam Inyang <saminyang09@gmail.com>
2022-07-11 18:28:55 -0400

    updating to CRA 5, and making necessary updates to support it

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app"],
   "framework": "@storybook/react",
   babel: {
-    plugins: [
-      ["babel-plugin-styled-components"]
-    ]
+    plugins: [["babel-plugin-styled-components"]]
+  },
+  core: {
+    builder: "webpack5"
   }
-}
+};
