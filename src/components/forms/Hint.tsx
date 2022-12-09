5eb5b8b096d1a7b1c262a1cdbb2e4e2265624616
Sam Inyang <saminyang09@gmail.com>
2022-12-09 11:19:57 -0500

    updating text area component

--- src/components/forms/Hint.tsx
+++ src/components/forms/Hint.tsx
@@ -1,12 +1,16 @@
-// @ts-ignore
-import { Hint as ZHint }            from "@zendeskgarden/react-forms"
-import styled                       from "styled-components"
-import { FONT_SIZES, FONT_WEIGHTS } from "styles/typography"
+import { Hint as ZHint } from "@zendeskgarden/react-forms"
+import styled            from "styled-components"
+import { CSSProp }       from "styles/types"
+import {
+  FONT_SIZES,
+  FONT_WEIGHTS
+}                        from "styles/typography"
 
-export const Hint = styled(ZHint)`
+export const Hint = styled(ZHint)<CSSProp>`
   &&& {
     font-size: ${FONT_SIZES.XS};
     font-weight: ${FONT_WEIGHTS.MEDIUM};
     color: ${({ theme }) => theme.styles.textColorSecondary};
+    ${({ _css }: CSSProp) => _css}
   }
 `
