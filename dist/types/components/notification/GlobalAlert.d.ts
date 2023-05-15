9b5302ea4c466882e7f32805500e356185f9aa78
Sam Inyang <saminyang09@gmail.com>
2023-05-15 00:08:55 -0400

    rollup

---
+++ dist/types/components/notification/GlobalAlert.d.ts
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from "react";
+import { UserFeedbackProps } from "styles/UserFeedbackProps";
+declare type Props = {
+    body: ReactNode;
+    notDismissible?: boolean;
+    noActionButton?: boolean;
+    showTitle?: boolean;
+    title?: string;
+    buttonText?: string;
+    onActionButtonClick?: () => void;
+    onClose?: () => void;
+} & UserFeedbackProps;
+export declare type GlobalAlertProps = Props;
+export declare const GlobalAlert: FC<Props>;
+export {};
+//# sourceMappingURL=GlobalAlert.d.ts.map
