b1f1bcc40868380cde3dbe7321ca9d531c3e1033
Sam Inyang <sinyang@users.noreply.github.com>
2023-10-06 17:31:43 +0000

    ensuring new line spaces are respected in timeline component

--- src/components/accordions/Timeline.tsx
+++ src/components/accordions/Timeline.tsx
@@ -1,7 +1,8 @@
 import { ReactNode, FC } from "react";
 import { css } from "styled-components"
-import { Timeline as ZTimeline } from "@zendeskgarden/react-accordions";
-import { Text } from "components/text/Text";
+import { Timeline as ZTimeline } from "@zendeskgarden/react-accordions"
+import { Text } from "components/text/Text"
+import { useTheme } from "styles/theme/useTheme"
 
 export type TimeLineItem = {
     content: string
@@ -15,22 +16,23 @@ export type TimelineProps = {
 }
 
 export const Timeline: FC<TimelineProps> = ({ isAlternate = false, data }) => {
+    const theme = useTheme()
 
     return (
-        <ZTimeline isAlternate = {isAlternate}>
+        <ZTimeline isAlternate={isAlternate}>
             {data.map((item) => {
                 return (
-                  <ZTimeline.Item icon = {item.icon ? <div>{item.icon}</div> : undefined}>
+                    <ZTimeline.Item icon={item.icon ? <div>{item.icon}</div> : undefined}>
                         <ZTimeline.Content>
-                            <Text>{item.content}</Text>
+                            <Text _css={css`white-space: pre-wrap;`}>{item.content}</Text>
                             {
                                 item.dateTime
-                                ?
-                                <Text _css = {css`color: #87929D`}>
-                                    {item.dateTime}
-                                </Text>
-                                :
-                                null
+                                    ?
+                                    <Text _css={css`color: ${theme.styles.textColorSecondary};`}>
+                                        {item.dateTime}
+                                    </Text>
+                                    :
+                                    null
                             }
                         </ZTimeline.Content>
                     </ZTimeline.Item>
