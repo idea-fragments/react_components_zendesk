a1b7cba303e906be99eeaaa2c292abd244fd822d
Sam Inyang <saminyang09@gmail.com>
2023-11-11 20:12:49 -0500

    allowing the timeline component to support custom content

--- src/components/accordions/Timeline.stories.tsx
+++ src/components/accordions/Timeline.stories.tsx
@@ -1,6 +1,10 @@
 import { mdiLeaf } from "@mdi/js"
 import { Timeline, TimelineProps } from "components/accordions/Timeline"
 import { Icon } from "components/Icon"
+import { FlexBox } from "components/layout/FlexBox"
+import { OverflowMenu } from "components/layout/OverflowMenu"
+import { ButtonLink } from "components/navigation/ButtonLink"
+import { Text } from "components/text/Text"
 import React from "react"
 
 export default {
@@ -19,14 +23,41 @@ const data = [
         svg={mdiLeaf}
       />
     ),
+    key: 1,
   },
   {
-    content: "good bye world",
+    content: (
+      <FlexBox>
+        <FlexBox withRows>
+          <Text>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Text>
+          <ButtonLink
+            external
+            href={"https://google.com"}
+            inline>
+            Some link
+          </ButtonLink>
+        </FlexBox>
+        <OverflowMenu
+          actions={[
+            { action: () => alert("action 1"), label: "Action 1" },
+            { action: () => alert("action 2"), label: "Action 2" },
+          ]}
+          appendToNode={document.body}
+        />
+      </FlexBox>
+    ),
     dateTime: "2023-10-21, 9:50 PM",
+    key: 2,
   },
   {
-    content: "The world is full of \ninjustice",
+    content: "The world is full of \nflowers",
     dateTime: "2023-10-21, 9:50 PM",
+    key: 3,
   },
 ]
 
